5981219c0f4175ab2cd4c813423900b436135297
Halit <60817499+halitos@users.noreply.github.com>
2020-06-05 17:36:54 +0100

    started some js events

---
+++ script.js
@@ -0,0 +1,11 @@
+const fontBtn = document.querySelector(
+  "#box-container > div.fontBox.box1 > button"
+);
+
+const textArea = document.querySelector(".entry");
+
+function lrgFont() {
+  textArea.style.fontSize = "2em";
+}
+
+fontBtn.addEventListener("click", lrgFont);
