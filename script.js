85d63f78cca60107bb8f018f3babf361048d2611
Halit <60817499+halitos@users.noreply.github.com>
2020-06-06 21:04:58 +0100

    added some more click events

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* 
 const fontBtn = document.querySelector(
   "#box-container > div.fontBox.box1 > button"
 );
@@ -9,3 +10,42 @@ function lrgFont() {
 }
 
 fontBtn.addEventListener("click", lrgFont);
+*/
+
+// ---------------Larger Font ---------------
+
+const textArea = document.querySelector(".entry");
+const LargeFontBtn = document.querySelector("#largeFont");
+
+function lrgFont() {
+  textArea.style.fontSize = "1.5em";
+}
+
+LargeFontBtn.addEventListener("click", lrgFont);
+
+//--------------------Smaller Font---------------
+
+const SmallFontBtn = document.querySelector("#smallFont");
+
+function SmlFont() {
+  textArea.style.fontSize = "0.8em";
+}
+
+SmallFontBtn.addEventListener("click", SmlFont);
+
+//--------------------Font Color--------------------
+
+const colorBtn = document.getElementById("fontColor");
+
+function buttonColors() {
+  textArea.style.color =
+    "rgb(" +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    ")";
+}
+
+colorBtn.addEventListener("click", buttonColors);
